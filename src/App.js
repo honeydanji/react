721bc951a2ddc22fd24576964f278078ed43973d
honeydanji <tjdwls3680@naver.com>
2023-04-21 13:08:35 +0900

    REACT(컴포넌트, provs)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 //import Hello from './01/Hello';
-import Myclock from './02/MyClock';
+//import Myclock from './02/MyClock';
+import Box from './03/Box';
 
 function App() {
   return (
     //<Hello />
-    <Myclock/>
+    //<Myclock/>
+    <Box/>
   );
 }
 
